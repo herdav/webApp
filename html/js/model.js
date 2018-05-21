94bc488810cd8247604552ac4d58e139c3e8e074
David Herren <herdav.mobile@gmail.com>
2018-05-21 08:05:28 +0200

    Create model.js

---
+++ html/js/model.js
@@ -0,0 +1,40 @@
+var museen = [
+	{
+		"bezeichnung": "",
+		"typ": "historisch, zeitgenössisch",
+		"beschreibung": "",
+		"adresse": "",
+		"ort": "",
+		"telefon": "",
+		"longitude": ,
+		"latitude": ,
+		"url": "",
+		"email": "",
+		"image": "",
+		"imageAktuell": "",
+		"eintritt": "",
+		"oeffnungszeiten": "",
+		"ausstellungDatum": "",
+		"ausstellungTitel": "",
+		"urlAusstellung": ""
+	},
+	{
+		"bezeichnung": "",
+		"typ": "historisch, zeitgenössisch",
+		"beschreibung": "",
+		"adresse": "",
+		"ort": "",
+		"telefon": "",
+		"longitude": ,
+		"latitude": ,
+		"url": "",
+		"email": "",
+		"image": "",
+		"imageAktuell": "",
+		"eintritt": "",
+		"oeffnungszeiten": "",
+		"ausstellungDatum": "",
+		"ausstellungTitel": "",
+		"urlAusstellung": ""
+	}
+];
