9f42123a88781198185bac212b754becdb09d837
David Herren <info@davidherren.ch>
2024-01-17 15:26:02 +0100

    Update index.php

--- db-based/httpdocs/index.php
+++ db-based/httpdocs/index.php
@@ -1,4 +1,4 @@
-<!--index.php for davidherren.ch / 2024-01-06-->
+<!--index.php for davidherren.ch / 2024-01-17-->
 <!DOCTYPE html>
 <html lang="de">
   <head>
@@ -9,33 +9,48 @@
     <script type="module" src="https://unpkg.com/@google/model-viewer@latest"></script>
   </head>
   <body>
-    <div id="header">
-      <a href="./">David Herren</a>
-      <div id="language-switch">
-        <button id="button-de" class="button-language" onclick="switchLanguage('de')">de</button>
-        <button id="button-en" class="button-language" onclick="switchLanguage('en')">en</button>
-      </div>
-    </div>
     <?php
     include 'data.php';
     $menuResult = $database->fetchMenuItems();
-    $indexResult = $database->fetchIndexItems(["motionstudy"]); // Exclude array of slugs from index site
+    $indexResult = $database->fetchIndexItems(["motionstudy"]);
     ?>
-    <div id="menu">
-      <div id="menu-works">
-        <?php
-        if (count($menuResult) > 0) {
-          foreach ($menuResult as $row) {
-            $slug = $row["slug"];
-            $title = $row["title"];
-            echo '<button id="button-' . htmlspecialchars($slug) . '" class="button-navigation" onclick="loadWorks(\'' . htmlspecialchars($slug) . '\')">' . htmlspecialchars($title) . '</button>' . PHP_EOL;
-          }
-        } else { echo "No Buttons found!"; }
-        ?>
+    <div id="menu" class="frame">
+      <div id="menu-title">
+        <div id="menu-title-left">
+          <button id="button-dropdown" class="button-navigation" onclick="dropDown()">
+          <svg id="dropdown-svg" width="40px" height="40px" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+            <polygon points="18,3 21,3 38,22 35,27 3,27 0,22 18,3"/>
+          </svg>
+          </button>
+        </div>
+        <a href="./" id="menu-title-center">
+          <div id="menu-title-center-inner">
+            <h1>David Herren</h1>
+          </div>
+        </a>
+        <div id="menu-title-right">
+          <div id="language-switch">
+            <button id="button-de" class="button-navigation" onclick="switchLanguage('de')">de</button>
+            <button id="button-en" class="button-navigation" onclick="switchLanguage('en')">en</button>
+          </div>
+        </div>
       </div>
-      <div id="menu-about">
-        <button id="button-exhibitions" class="button-navigation" onclick="loadExhibitions()">Exhibitions</button>
-        <button id="button-about" class="button-navigation" onclick="loadAbout()">About</button>
+      <div id="menu-inner">
+        <div id="menu-works">
+          <?php
+          if (count($menuResult) > 0) {
+            foreach ($menuResult as $row) {
+              $slug = $row["slug"];
+              $title = $row["title"];
+              echo '<button id="button-' . htmlspecialchars($slug) . '" class="button-navigation" onclick="loadWorks(\'' . htmlspecialchars($slug) . '\')">' . htmlspecialchars($title) . '</button>' . PHP_EOL;
+            }
+          } else { echo "No Buttons found!"; }
+          ?>
+        </div>
+        <div id="menu-about">
+          <!--<button id="button-exhibitions" class="button-navigation" onclick="loadExhibitions()">Exhibitions</button>-->
+          <button id="button-about" class="button-navigation" onclick="loadAbout()">About</button>
+        </div>
       </div>
     </div>
     <div id="index">
