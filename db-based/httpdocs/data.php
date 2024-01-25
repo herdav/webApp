87c77b2afe6bff5619a90e5e93327fd36c621743
David Herren <info@davidherren.ch>
2024-01-25 17:14:01 +0100

    Update data.php

--- db-based/httpdocs/data.php
+++ db-based/httpdocs/data.php
@@ -1,4 +1,4 @@
-<?php // data.php for davidherren.ch / 2024-01-20
+<?php // data.php for davidherren.ch / 2024-01-24
 
 include './php/db.php';
 include './php/fetchData.php';
@@ -91,12 +91,6 @@ if (isset($_GET['works']) && isset($_GET['lang']) && isset($_GET['slug'])) {
     }
     $htmlOutput .= '</div>';
 
-    if ($workData["github"]) {
-      $htmlOutput .= "<a id='github-link' href='" . htmlspecialchars($workData["github"]) . "' target='_blank'>&lt;&sol;&gt;</a>";
-    }
-
-    $htmlOutput .= '</div>';
-
     // Code for displaying additional images
     $htmlOutput .= "<div class='work-images'>";
     foreach ($workimages as $image) {
@@ -113,6 +107,12 @@ if (isset($_GET['works']) && isset($_GET['lang']) && isset($_GET['slug'])) {
       $htmlOutput .= <<<HTML
       <model-viewer id="embedded-model" src="/3d/{$slug}.gltf" alt="Ein 3D-Modell" camera-controls touch-action="pan-y" tone-mapping="agx" exposure="0.5" autoplay ar ar-modes="webxr scene-viewer" shadow-intensity="0" shadow-softness="1.5"></model-viewer>
       HTML;
+    }
+
+    if ($workData["github"]) {
+      $htmlOutput .= "<a id='github-link' href='" . htmlspecialchars($workData["github"]) . "' target='_blank'>";
+      $htmlOutput .= "<svg width='98' height='96' mlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z'/></svg>";
+      $htmlOutput .= "</a></div>";
     }
 
     // Code for Vimeo iframes
@@ -232,7 +232,7 @@ if (isset($_GET['about']) && isset($_GET['lang'])) {
     } else {
       $htmlOutput .= "<h2><mark>Exhibitions</mark></h2>";
     }
-    foreach ($exhibitionData as $exhibition) {
+    foreach ($exhibitionData as $index => $exhibition) {
       $yearStart = substr($exhibition["date_start"], 0, 4);
       $htmlOutput .= "<div class='about-items' style='display: flex;'>";
       if ($yearStart !== $lastYear) {
@@ -241,7 +241,20 @@ if (isset($_GET['about']) && isset($_GET['lang'])) {
       } else {
         $htmlOutput .= "<div style='flex: 1;'></div>";
       }
-      $htmlOutput .= "<div style='flex: 3;'>";
+      $htmlOutput .= "<div style='flex: 3;'><div class='exhibition-sub-main'>";
+
+      $buttonId = "button-dropdown-" . $index;
+      $htmlOutput .= "<button id='" . $buttonId . "' class='button-navigation-about' onclick=\"expandImage('" . htmlspecialchars($exhibition['img']) . "', '" . $buttonId . "'); toggleTriangles('" . $buttonId . "');\">
+        <svg id='dropdown-svg' width='64px' height='32px' viewBox='0 0 64 32' xmlns='http://www.w3.org/2000/svg'>
+          <rect x='4'  y='24' id='svg-triangle-1' width='56' height='4'/>
+          <rect x='8'  y='20' id='svg-triangle-2' width='48'  height='4'/>
+          <rect x='12' y='16' id='svg-triangle-3' width='40' height='4'/>
+          <rect x='16' y='12' id='svg-triangle-4' width='32' height='4'/>
+          <rect x='20' y='8'  id='svg-triangle-5' width='24' height='4'/>
+          <rect x='24' y='4'  id='svg-triangle-6' width='16' height='4'/>
+          <rect x='28' y='0'  id='svg-triangle-7' width='8'  height='4'/>
+        </svg>
+      </button>";
 
       $firstLink = $database->fetchExhibitionMainLink($exhibition['title']);
       if ($firstLink) {
@@ -249,10 +262,17 @@ if (isset($_GET['about']) && isset($_GET['lang'])) {
       } else {
           $htmlOutput .= "<p>"  . $exhibition['title'] . ", ";
       }
-      $htmlOutput .= $exhibition['institution'];
-      $htmlOutput .= ", " . htmlspecialchars($exhibition['place']) . "</p>";
-      $htmlOutput .= "<div class='exhibition-img'><img src=/img/exhibitions/" . htmlspecialchars($exhibition['img']) . "></div>";
-      $htmlOutput .= "</div></div>";
+      $htmlOutput .= $exhibition['institution'] . ', ' . $exhibition["place_$lang"] . "</p></div>";
+      
+      $htmlOutput .= "<div id='" . htmlspecialchars($exhibition['img']) . "' class='exhibition-img'><img src='/img/exhibitions/" . htmlspecialchars($exhibition['img']) . ".jpg'>";
+      $htmlOutput .= '<p>' . htmlspecialchars($exhibition["text_$lang"]) . '<br><br>';
+      $dateObjectStart = new DateTime($exhibition["date_start"]);
+      $dateObjectEnd = new DateTime($exhibition["date_end"]);
+      $formattedDateStart = $dateObjectStart->format('d.m.');
+      $formattedDateEnd = $dateObjectEnd->format('d.m.Y');
+      $htmlOutput .= htmlspecialchars($formattedDateStart) . ' &mdash; ';
+      $htmlOutput .= htmlspecialchars($formattedDateEnd) . '</p>';
+      $htmlOutput .= "</div></div></div>";
     }
     $htmlOutput .= "</div>"; // End of Exhibition section
   }
