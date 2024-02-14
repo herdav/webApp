db3ed09f1d3eb30a36132e219d03a45869c5eac0
David Herren <info@davidherren.ch>
2024-02-14 12:36:17 +0100

    Update data.php

--- db-based/httpdocs/data.php
+++ db-based/httpdocs/data.php
@@ -1,4 +1,4 @@
-<?php // data.php for davidherren.ch / 2024-02-02
+<?php // data.php for davidherren.ch / 2024-02-14
 
 include './php/db.php';
 include './php/fetchData.php';
@@ -130,6 +130,10 @@ if (isset($_GET['works']) && isset($_GET['lang']) && isset($_GET['slug'])) {
     foreach ($worklinks as $link) {
       $htmlOutput .= "<a href='" . htmlspecialchars($link["address"]) . "' target='_blank'>" . htmlspecialchars($link["description"]) . "</a> ";
     }
+    if ($workData["github"]) {
+      $htmlOutput .= "<a href='" . htmlspecialchars($workData["github"]) . "' target='_blank'>GitHub</a> ";
+    }
+
     $htmlOutput .= '</div>';
 
     // Code for displaying additional images
@@ -141,10 +145,12 @@ if (isset($_GET['works']) && isset($_GET['lang']) && isset($_GET['slug'])) {
       $imageAlt    = htmlspecialchars($image["alt"]);
       $imageFoto   = htmlspecialchars($image["foto"]);
       $imageCopyr  = htmlspecialchars($image["copyright"]);
+      $imageNumber = htmlspecialchars($image["number"]);
+
       if ($imageFoto) {
-        $htmlOutput .= "<img src='/img/{$imageName}.{$imageType}' alt='$workTitle - Foto: $imageFoto'>";
+        $htmlOutput .= "<div class='abb-anchor' id='_abb{$imageNumber}'></div><img src='/img/{$imageName}.{$imageType}' alt='$workTitle - Foto: $imageFoto'>";
       } else {
-        $htmlOutput .= "<img src='/img/{$imageName}.{$imageType}' alt='David Herren'>";
+        $htmlOutput .= "<div class='abb-anchor' id='_abb{$imageNumber}'></div><img id='_abb{$imageNumber}' src='/img/{$imageName}.{$imageType}' alt='David Herren'>";
       }
     }
 
@@ -155,12 +161,6 @@ if (isset($_GET['works']) && isset($_GET['lang']) && isset($_GET['slug'])) {
       $htmlOutput .= <<<HTML
       <model-viewer id="embedded-model" src="/3d/{$slug}.gltf" alt="Ein 3D-Modell" camera-controls touch-action="pan-y" tone-mapping="agx" exposure="0.5" autoplay ar ar-modes="webxr scene-viewer" shadow-intensity="0" shadow-softness="1.5"></model-viewer>
       HTML;
-    }
-
-    if ($workData["github"]) {
-      $htmlOutput .= "<a id='github-link' href='" . htmlspecialchars($workData["github"]) . "' target='_blank'>";
-      $htmlOutput .= "<svg width='98' height='96' mlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z'/></svg>";
-      $htmlOutput .= "</a></div>";
     }
 
     // Code for Vimeo iframes
@@ -266,7 +266,7 @@ if (isset($_GET['about']) && isset($_GET['lang'])) {
       $htmlOutput .= "<div style='flex: 3;'><div class='exhibition-sub-main'>";
 
       $buttonId = "button-dropdown-" . $index;
-      $htmlOutput .= "<button id='" . $buttonId . "' class='button-navigation-about' onclick=\"expandImage('" . htmlspecialchars($exhibition['img']) . "', '" . $buttonId . "'); toggleTriangles('" . $buttonId . "');\">
+      $htmlOutput .= "<button id='" . $buttonId . "' class='button-navigation exhibition' onclick=\"expandImage('" . htmlspecialchars($exhibition['img']) . "', '" . $buttonId . "'); toggleTriangles('" . $buttonId . "');\">
         <svg id='dropdown-svg' width='64px' height='32px' viewBox='0 0 64 32' xmlns='http://www.w3.org/2000/svg'>
           <rect x='4'  y='24' id='svg-triangle-1' width='56' height='4'/>
           <rect x='8'  y='20' id='svg-triangle-2' width='48'  height='4'/>
